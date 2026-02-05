10b130e956a3883cecc02dafc2f0089fff228c39
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2026-02-05 14:29:07 +0100

    Initial commit

---
+++ src/main/java/se/magnus/jmsdbexample/JmsDbExampleApplication.java
@@ -0,0 +1,13 @@
+package se.magnus.jmsdbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsDbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsDbExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/magnus/jmsdbexample/JmsDbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package se.magnus.jmsdbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsDbExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
